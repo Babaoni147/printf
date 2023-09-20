d4327e470fef5133a3464e507426386d7c6e308a
Obah_Edwin <obahedwin@ymail.com>
2023-09-19 19:24:04 -0700

    sets flags if _printf finds

---
+++ set_flags.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* set_flags - sets flags if _printf finds
+* @s: character that holds the flag specifier
+* @f: pointer to the struct fmt_flags in which we set the flags
+*
+* Return: 1 if a flag has been set, 0 otherwise
+*/
+int set_flags(char s, fmt_flags_t *f)
+{
+int flag_set = 0;
+
+if (s == '+')
+{
+f->plus = 1;
+flag_set = 1;
+}
+else if (s == ' ')
+{
+f->space = 1;
+flag_set = 1;
+}
+else if (s == '#')
+{
+f->hash = 1;
+flag_set = 1;
+}
+
+return (flag_set);
+}
