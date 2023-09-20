6789e064efd4e772369b63ac5cba95ff479c1f2f
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-20 13:36:56 -0700

    for _printf.c task

--- _printf.c
+++ _printf.c
@@ -1,22 +1,20 @@
 #include "main.h"
 /**
-* _printf - printf function
-* @format: const char pointer
-* Return: b_len
+* _printf - Custom printf function
+* @format: Format string
+* Return: Number of characters printed
 */
 int _printf(const char *format, ...)
 {
-int (*pfunc)(va_list, fmt_flags_t *);
+int (*print_fun)(va_list, fmt_flags_t *);
 const char *p;
-va_list arguments;
+va_list args;
 fmt_flags_t flags = {0, 0, 0};
+int chars_printed = 0;
 
-int _reg = 0;
-
-va_start(arguments, format);
-if (!format || (format[0] == '%' && !format[1]))
-return (-1);
-if (format[0] == '%' && format[1] == ' ' && !format[2])
+va_start(args, format);
+if (!format || (format[0] == '%' && !format[1]) ||
+(format[0] == '%' && format[1] == ' ' && !format[2]))
 return (-1);
 for (p = format; *p; p++)
 {
@@ -25,29 +23,26 @@ if (*p == '%')
 p++;
 if (*p == '%')
 {
-_reg += _putchar('%');
+chars_printed += _putchar('%');
 continue;
 }
 while (get_flag(*p, &flags))
 p++;
 if (*p == '%')
 {
-_reg += _putchar('%');
+chars_printed += _putchar('%');
 continue;
 }
 while (get_flag(*p, &flags))
 p++;
-pfunc = get_print(*p);
-_reg += (pfunc)
-? pfunc(arguments, &flags)
-: _printf("%%%c", *p);
+print_fun = get_print(*p);
+chars_printed += (print_fun) ? print_fun(args, &flags) :
+_printf("%%%c", *p);
 }
 else
-{
-_reg += _putchar(*p);
-}
+chars_printed += _putchar(*p);
 }
 _putchar(-1);
-va_end(arguments);
-return (_reg);
+va_end(args);
+return (chars_printed);
 }
